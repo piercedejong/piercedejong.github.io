f493d95684b6eec135c716e8e58b00ea647b8695
piercedejong <pierce.dejong45@gmail.com>
2019-11-18 18:58:57 -0700

    Added backgorund iamge

--- _site/javascript/index.js
+++ _site/javascript/index.js
@@ -1,4 +1,5 @@
 function includeHTML() {
+  console.log("hello");
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
